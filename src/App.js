d82f122450a08414cbeab592f36d383774ed9035
ans <answaheed1@gmail.com>
2022-10-04 19:28:40 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Mentor />
+    </div>
+  );
+}
+
+function Mentor() {
+
+
+  return (
+  <div>
+
+  <h1 className = "bg-slate-500">Notifications 3 Mark all as read</h1>
+
+  <ul>
+      <li>Mark Webber reacted to your recent post My first tournament today!
+      1m ago</li>
+
+      <li>Angela Gray followed you
+      5m ago</li>
+
+      <li>Jacob Thompson has joined your group Chess Club
+      1 day ago</li>
+
+      <li>Rizky Hasanuddin sent you a private message
+      5 days ago Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+      I'm already having lots of fun and improving my game.
+      </li>
+
+      
+      <li>Kimberly Smith commented on your picture
+      1 week ago</li>
+
+      <li>Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
+      2 weeks ago</li>
+
+      <li>Anna Kim left the group Chess Club
+      2 weeks ago</li>
+  </ul>
+  </div>
+  );
+}
