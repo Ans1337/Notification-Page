1e803aa66ec3e23b5a2107d3afaf0d82a579d575
ans <answaheed1@gmail.com>
2022-10-15 22:27:07 +0500

    v0.4

--- src/App.js
+++ src/App.js
@@ -13,63 +13,67 @@ export default function App() {
 
 function User(){
   const [notificationCount,setNotificationCount] = useState(0);
+  
   let [userData,setUserData] = useState([{
-    id: 1,
-    avatar: 'avatar-mark-webber.webp',
-    name: 'Mark Webber',
-    activity: 'Reacted to your recent post',
-    interaction:  'My first tournament today',
-    time: '1m ago',
-    active: true
-},
-{
-    id: 2,
-    avatar: 'avatar-angela-gray.webp',
-    name: 'Angela Gray',
-    activity: 'followed you',
-    time: '5m ago',
-    active: true
-},
-{
-    id: 3,
-    avatar: 'avatar-jacob-thompson.webp',
-    name: 'Jacob Thompson',
-    activity: 'has joined your group',
-    interaction:  'Chess Club',
-    time: '1day ago',
-    active: true
-},
-{
-    id: 4,
-    avatar: 'avatar-rizky-hasanuddin.webp',
-    name: 'Rizky Hasanuddin',
-    activity: 'sent you a private message',
-    privateMessage:  'Hello thanks for setting up the Chess Club. I\'ve been a member for a few weeks now and I\'m already having lots of fun and improving my game.',
-    time: '5days ago'
-},
-{
-    id: 5,
-    avatar: 'avatar-kimberly-smith.webp',
-    name: 'Kimberly Smith',
-    activity: 'commented on your picture',
-    time: '1weeks ago'
-},
-{
-    id: 6,
-    avatar: 'avatar-nathan-peterson.webp',
-    name: 'Nathan Peterson',
-    activity: 'Reacted to your recent post',
-    interaction:  '5end-game strategies to increase you win rate',
-    time: '2weeks ago'
-},
-{   
-    id: 7,
-    avatar: 'avatar-anna-kim.webp',
-    name: 'Anna Kim',
-    activity: 'left the group',
-    interaction:  'Chess Club',
-    time: '2weeks ago'
-},]);
+      id: 1,
+      avatar: 'avatar-mark-webber.webp',
+      name: 'Mark Webber',
+      activity: 'Reacted to your recent post',
+      interaction:  'My first tournament today',
+      time: '1m ago',
+      active: true
+  },
+  {
+      id: 2,
+      avatar: 'avatar-angela-gray.webp',
+      name: 'Angela Gray',
+      activity: 'followed you',
+      time: '5m ago',
+      active: true
+  },
+  {
+      id: 3,
+      avatar: 'avatar-jacob-thompson.webp',
+      name: 'Jacob Thompson',
+      activity: 'has joined your group',
+      interaction:  'Chess Club',
+      time: '1day ago',
+      active: true
+  },
+  {
+      id: 4,
+      avatar: 'avatar-rizky-hasanuddin.webp',
+      name: 'Rizky Hasanuddin',
+      activity: 'sent you a private message',
+      privateMessage:  'Hello thanks for setting up the Chess Club. I\'ve been a member for a few weeks now and I\'m already having lots of fun and improving my game.',
+      time: '5days ago',
+      active: false
+  },
+  {
+      id: 5,
+      avatar: 'avatar-kimberly-smith.webp',
+      name: 'Kimberly Smith',
+      activity: 'commented on your picture',
+      activitypic: 'image-chess.webp',
+      time: '1weeks ago'
+  },
+  {
+      id: 6,
+      avatar: 'avatar-nathan-peterson.webp',
+      name: 'Nathan Peterson',
+      activity: 'Reacted to your recent post',
+      interaction:  '5end-game strategies to increase you win rate',
+      time: '2weeks ago'
+  },
+  {   
+      id: 7,
+      avatar: 'avatar-anna-kim.webp',
+      name: 'Anna Kim',
+      activity: 'left the group',
+      interaction:  'Chess Club',
+      time: '2weeks ago'
+  },
+]);
 
   function calculateCount() {
     let temp = 0
@@ -105,7 +109,12 @@ function User(){
       <div className={`p-6 ${userData.active === true ? 'rounded-xl bg-sky-200' : ''}`}>
         <img className="float-left pr-4" src={userData.avatar} alt="Avatar" width="65" height="65"></img>
         <div>
-          <p> <b>{userData.name}</b> {userData.activity} <i>{userData.interaction}</i></p>
+          <p> 
+            <b className="hover:text-blue-900">{userData.name}</b>
+             {userData.activity}
+             {userData.activitypic ? <img className="float-right pl-4 hover:scale-110" src={userData.activitypic} alt="Avatar" width="65" height ="65"></img> : <></>}
+             <b className="hover:text-blue-900">{userData.interaction}</b>
+          </p>
           <p className="pl-16 pb-2">{userData.time}</p>
           <div className="pl-16">
             {userData.privateMessage ? <p className="p-2 text-justify rounded-lg border-2 border-gray-400 hover:bg-blue-100">{userData.privateMessage}</p> : <></>}
@@ -122,7 +131,7 @@ function User(){
        <ul>
          <li>
            <div className="pb-10">
-             <p className="float-left text-2xl font-bold">Notification 
+             <p className="float-left text-2xl font-bold">Notifications  { }
               <span className="rounded-lg bg-blue-700 text-white">{notificationCount}</span>
              </p>
              <button onClick={function(event){ readNotification(); calculateCount()}}  className="float-right flex text-xl hover:text-blue-400">Mark all as read</button>
